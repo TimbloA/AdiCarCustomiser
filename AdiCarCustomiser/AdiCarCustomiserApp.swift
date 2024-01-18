bbfb87aea7d8302fbda667b931e2b3f1a86a179f
Adi Timblo <timblo.a@etoncollege.org.uk>
2024-01-18 20:51:28 +0000

    Initial Commit

---
+++ AdiCarCustomiser/AdiCarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdiCarCustomiserApp.swift
+//  AdiCarCustomiser
+//
+//  Created by Adi Timblo on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AdiCarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
